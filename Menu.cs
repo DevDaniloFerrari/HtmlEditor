b4e658dcc94461045843f3f819c497c28507a442
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2023-03-28 08:50:42 -0300

    initial commit

---
+++ Menu.cs
@@ -0,0 +1,9 @@
+public static class Menu
+{
+    public static void Show()
+    {
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.ForegroundColor = ConsoleColor.Black;
+    }
+}
