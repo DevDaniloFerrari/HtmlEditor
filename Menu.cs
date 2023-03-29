4ccd665f21dd298cdd51e41f3c91ae6c894047db
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2023-03-29 08:33:26 -0300

    implement draw screen

--- Menu.cs
+++ Menu.cs
@@ -5,5 +5,41 @@ public static class Menu
         Console.Clear();
         Console.BackgroundColor = ConsoleColor.Blue;
         Console.ForegroundColor = ConsoleColor.Black;
+
+        DrawScreen();
     }
+
+    public static void DrawScreen()
+    {
+        DrawLine();
+
+        DrawSpace(10);
+
+        DrawLine();
+    }
+
+    private static void DrawSpace(int total)
+    {
+        for (int lines = 0; lines <= total; lines++)
+        {
+            Console.Write("|");
+            for (int i = 0; i < 30; i++)
+                Console.Write(" ");
+            Console.Write("|");
+
+            BreakLine();
+        }
+    }
+
+    private static void DrawLine()
+    {
+        Console.Write("+");
+        for (int i = 0; i < 30; i++)
+            Console.Write("-");
+        Console.Write("+");
+
+        BreakLine();
+    }
+
+    private static void BreakLine() => Console.Write(Environment.NewLine);
 }
